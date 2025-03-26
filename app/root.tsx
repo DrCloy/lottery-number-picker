34fbc6fb372c492ed58ba064e61ffc58784ca87f
MinGyo Kim <110979416+DrCloy@users.noreply.github.com>
2025-03-26 17:01:31 +0900

    Update style in root

--- app/root.tsx
+++ app/root.tsx
@@ -32,7 +32,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body className="flex flex-col min-h-screen">
+      <body className="flex flex-col items-center min-h-screen min-w-md">
         {children}
         <ScrollRestoration />
         <Scripts />
